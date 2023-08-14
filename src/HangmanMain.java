c60c769dd041dbd5c3f98e1c5a2845b62e171557
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-08-14 14:13:06 -0600

    Create HangmanMain.java

---
+++ src/HangmanMain.java
@@ -0,0 +1,18 @@
+public class HangmanMain {
+    
+    /**
+     * Your main method must do the following:
+     * * Create a new HangmanGame object
+     * * Call the printScreen() method (remember how to call methods on different objects!)
+     * * Call initializeWords(), passing in a fileName, which requires the input from the user
+     * * Call wordSelect().
+     * * Print the initial hangman gallows by calling getZero().
+     * * Call printWordProgress()
+     * * While the game is not finished, get input from the user on what letter they guess 
+     * using getInput("Guess a letter: ") and call guess(), passing in the guess input.
+     * @param args
+     */
+    public static void main(String[] args) {
+        //TODO Student
+    }
+}
